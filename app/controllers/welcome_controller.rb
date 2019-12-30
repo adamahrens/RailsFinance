a762caaac0d3f661a50aaa1d8512c9a35b579fba
Adam Ahrens <adam.aahrens@gmail.com>
2019-12-29 20:25:51 -0600

    Rescuing error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,16 @@ class WelcomeController < ApplicationController
   def index
     if params[:stocks]
       logger.debug "Fetching #{params[:stocks]} stock"
-      stock = StockQuote::Stock.quote(params[:stocks])
-      logger.debug "#{stock.symbol} - #{stock.company_name}: $#{stock.latest_price}"
-      @stock = Stock.new(ticker: stock.symbol,
-                         name: stock.company_name,
-                         price: stock.latest_price)
+      begin
+        stock = StockQuote::Stock.quote(params[:stocks])
+        logger.debug "#{stock.symbol} - #{stock.company_name}: $#{stock.latest_price}"
+        @stock = Stock.new(ticker: stock.symbol,
+                           name: stock.company_name,
+                           price: stock.latest_price)
+      rescue RuntimeError => e
+        logger.error e
+        logger.debug "Unable to fetch #{params[:stocks]} stock"
+      end
     end
   end
 end
